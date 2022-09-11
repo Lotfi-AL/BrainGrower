41e4baff45abe87526ceb550f870c79432a3498e
vegarrsm <arnedeig@gmail.com>
2022-09-11 20:12:41 +0200

    removed unused code

--- screens/SwitchGame.tsx
+++ screens/SwitchGame.tsx
@@ -1,10 +1,6 @@
-import { useNavigation } from '@react-navigation/native';
-
 import { Text, View } from '../components/Themed';
 
 export default function SwitchGameScreen() {
-
-  const nav = useNavigation();
 
   return (
     <View>
